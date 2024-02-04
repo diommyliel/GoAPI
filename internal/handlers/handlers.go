e559568b92875a2ba864403f97fa285e87b14e03
diommyliel <thomascoudurie@gmail.com>
2024-02-04 15:15:14 -0500

    update handlers

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/player", func(router chi.Router) {
+
+		router.Get("/counter", GetPlayerCounter)
+	})
+}
